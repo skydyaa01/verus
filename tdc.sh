b25e7f33cc631f9a30cfa5832395d4cdd09f7e93
skydyaa01 <98868557+skydyaa01@users.noreply.github.com>
2022-02-02 12:51:31 +0700

    Create tdc.sh

---
+++ tdc.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yespowertide -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TPenmCpKRGYsPwUXD5PCLAyGn6uc8E29sw.Pusing_Siti_Suhuna_maredit -t 15
